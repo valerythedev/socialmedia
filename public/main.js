2e225307771d71cd9b825a2967c2019880dceb81
valerythedev <valgonzr@gmail.com>
2023-04-30 21:10:34 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,98 @@
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down")
+var trash = document.getElementsByClassName("fa-trash");
+// let comment = document.getElementsByClassName('commenting')
+
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    fetch('messages/thumbDown', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbUp':thumbUp
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+let comment = document.getElementsByClassName('commenting')
+Array.from(comment).forEach(function(element) {
+  console.log(['element: ', element])
+  element.addEventListener('click', function(){
+    
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const text = this.parentNode.querySelector('.commentText').value
+    // use debugger to fix the lines and make sure it's identifying 
+    fetch('/messages/comment', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'comment':text
+      })
+  
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('/messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
